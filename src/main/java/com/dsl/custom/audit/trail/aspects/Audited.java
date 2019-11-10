b26d19d988adce4177d148d06d17c3ea98514e04
dslsyeoh <syeoh91x@gmail.com>
2019-11-10 19:07:08 +0800

    add custom annotation Audited

---
+++ src/main/java/com/dsl/custom/audit/trail/aspects/Audited.java
@@ -0,0 +1,18 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.custom.audit.trail.aspects;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Audited
+{
+    String value() default "";
+
+    Class<?> target();
+}
